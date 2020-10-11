10b9c778bb6d60ed185d2e04ff6b6b1a30645945
Jake Sichley <39101429+JakeSichley@users.noreply.github.com>
2020-10-11 11:16:21 -0700

    Implement Size, Coffee, Flavor, Strength Endpoints
    Implement remaining get, delete methods

--- scripts/index.js
+++ scripts/index.js
@@ -33,14 +33,16 @@ app.delete('/coffeeorders', (req, res) => {
         });
 });
 
-// email routes.
 app.get('/coffeeorders/:emailAddress', (req, res) => {
     const emailAddress = req.params.emailAddress;
     console.log(`looking for: ${emailAddress}`);
     db.connect('./data', ['coffeeorders']);
     let record = db.coffeeorders.find( { emailAddress: emailAddress } );
-    if (record) res.status(200).json(record);
-    else res.sendStatus(404);
+    if (record) {
+        res.status(200).json(record);
+    } else {
+        res.sendStatus(404);
+    }
 });
 
 app.put('/coffeeorders/:emailAddress', (req, res) => {
@@ -72,8 +74,112 @@ app.delete('/coffeeorders/:emailAddress', (req, res) => {
     if (record) {
         db.coffeeorders.remove( { _id: record._id }, false );
         res.sendStatus(200);
+    } else {
+        res.sendStatus(404);
     }
+});
+
+app.get('/coffeeorders/strength/:strength', (req, res) => {
+    const strength = parseInt(req.params.strength);
+    if (isNaN(strength)) {
+        res.sendStatus(404);
+    }
+    console.log(`looking for: ${strength}`);
+    db.connect('./data', ['coffeeorders']);
+    let record = db.coffeeorders.find( { strength: strength } );
+    if (record) {
+        res.status(200).json(record);
+    } else {
+        res.sendStatus(404);
+    }
+});
+
+app.delete('/coffeeorders/strength/:strength', (req, res) => {
+    const strength = parseInt(req.params.strength);
+    if (isNaN(strength)) {
+        res.sendStatus(404);
+    }
+    console.log(`looking for: ${strength}`);
+    db.connect('./data', ['coffeeorders']);
+    let record = db.coffeeorders.find( { strength: strength } );
+    if (record) {
+        db.coffeeorders.remove( { _id: order._id }, true );
+        res.sendStatus(200);
+    } else {
+        res.sendStatus(404);
+    }
+});
+
+app.get('/coffeeorders/flavor/:flavor', (req, res) => {
+    const flavor = req.params.flavor;
+    console.log(`looking for: ${flavor}`);
+    db.connect('./data', ['coffeeorders']);
+    let record = db.coffeeorders.find( { flavor: flavor } );
+    if (record) res.status(200).json(record);
     else res.sendStatus(404);
 });
- 
+
+app.delete('/coffeeorders/flavor/:flavor', (req, res) => {
+    const flavor = req.params.flavor;
+    console.log(`looking for: ${flavor}`);
+    db.connect('./data', ['coffeeorders']);
+    let record = db.coffeeorders.find( { flavor: flavor } );
+    if (record) {
+        db.coffeeorders.remove( { _id: order._id }, true );
+        res.sendStatus(200);
+    } else {
+        res.sendStatus(404);
+    }
+});
+
+app.get('/coffeeorders/size/:size', (req, res) => {
+    const size = req.params.size;
+    console.log(`looking for: ${size}`);
+    db.connect('./data', ['coffeeorders']);
+    let record = db.coffeeorders.find( { size: size } );
+    if (record) {
+        res.status(200).json(record);
+    } else {
+        res.sendStatus(404);
+    }
+});
+
+app.delete('/coffeeorders/size/:size', (req, res) => {
+    const size = req.params.size;
+    console.log(`looking for: ${size}`);
+    db.connect('./data', ['coffeeorders']);
+    let record = db.coffeeorders.find( { size: size } );
+    if (record) {
+        db.coffeeorders.remove( { _id: order._id }, true );
+        res.sendStatus(200);
+    } else {
+        res.sendStatus(404);
+    }
+});
+
+app.get('/coffeeorders/coffee/:coffee', (req, res) => {
+    const coffee = req.params.coffee;
+    console.log(`looking for: ${coffee}`);
+    db.connect('./data', ['coffeeorders']);
+    let record = db.coffeeorders.find( { coffee: coffee } );
+    if (record) {
+        res.status(200).json(record);
+    } else {
+        res.sendStatus(404);
+    }
+});
+
+app.delete('/coffeeorders/coffee/:coffee', (req, res) => {
+    const coffee = req.params.coffee;
+    console.log(`looking for: ${coffee}`);
+    db.connect('./data', ['coffeeorders']);
+    let record = db.coffeeorders.find( { coffee: coffee } );
+    if (record) {
+        db.coffeeorders.remove( { _id: order._id }, false );
+        res.sendStatus(200);
+    } else {
+        res.sendStatus(404);
+    }
+});
+
 app.listen(3000);
